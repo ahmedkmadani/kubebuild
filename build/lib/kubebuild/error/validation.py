2d3467847e673e1837569187e89a01b5e09bd94e
ahmedkmadani <ahmedkamal543@gmail.com>
2023-09-11 01:16:09 +0300

    add long_description

--- build/lib/kubebuild/error/validation.py
+++
@@ -1,8 +0,0 @@
-class InputValidator:
-    @staticmethod
-    def is_integer(input_value):
-        try:
-            int(input_value)
-            return True
-        except ValueError:
-            return False
